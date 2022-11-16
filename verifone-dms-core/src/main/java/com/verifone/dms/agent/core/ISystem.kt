d93a1840ff02e3185b7415c8988fc78f672dd2bd
Wendell Malpas <wendell.malpas@gmail.com>
2022-11-15 23:41:19 -0800

    Added new interface to get firmware version

--- verifone-dms-core/src/main/java/com/verifone/dms/agent/core/ISystem.kt
+++ verifone-dms-core/src/main/java/com/verifone/dms/agent/core/ISystem.kt
@@ -1,8 +1,7 @@
 package com.verifone.dms.agent.core
 
 interface ISystem {
-    fun getModel()
-    fun getSerialNumber()
-    fun installPackage(filePath:String)
-    fun uninstallPackage(filePath:String)
+    fun getModel():String
+    fun getSerialNumber():String
+    fun getFirmwareVersion():Int
 }
